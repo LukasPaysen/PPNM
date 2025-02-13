3c785d3ece83edb1ca3c0d5c1a1f36851530d35d
Your Name <you@example.com>
2025-02-13 19:43:20 +0100

    hello

--- Exercises/HelloWorld/main.cs
+++ Exercises/HelloWorld/main.cs
@@ -1,7 +1,6 @@
-class math{
-    static int Main(){
-	double sqrt2=System.Math.Sqrt(2.0);
-    System.Console.WriteLine($"Sqrt(2) = {sqrt2}");
+class hello{
+static int Main(){
+	System.Console.WriteLine("Hello, World!");
 	return 0;
 	}
 }
